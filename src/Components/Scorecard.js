0bd82dcc8ea53041944e51edabd1beb14d97715f
TamHOD <thomashodonnell@gmail.com>
2020-01-07 16:38:43 +0000

    score showing under par, over par and even at appropriate time

--- src/Components/Scorecard.js
+++ src/Components/Scorecard.js
@@ -14,6 +14,23 @@ const Scorecard = props => {
     );
   });
 
+  let runningTotal = 0;
+
+  props.scores.forEach((score, index) => {
+    const total = runningTotal - (score - props.pars[index]);
+    runningTotal = total;
+  });
+
+  let score = "";
+
+  if (runningTotal === 0) {
+    score = "Even";
+  } else if (runningTotal < 0) {
+    score = `+${runningTotal.toString()}`;
+  } else {
+    score = `-${Math.abs(runningTotal).toString()}`;
+  }
+
   return (
     <div className="scorecard">
       <h1>Scorecard</h1>
@@ -25,7 +42,10 @@ const Scorecard = props => {
         </tr>
         <tbody>{scoreRows}</tbody>
       </table>
-      <h1>Score</h1>
+      <h1>
+        Score:
+        {score}
+      </h1>
       <Link to="/score">
         <button className="next-hole-button">Next Hole</button>
       </Link>
